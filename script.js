c158086749913f688a0da86240ccf98860907c92
Agnibhu Mandal <agnibhu.mandal@gmail.com>
2023-03-22 16:14:39 +0530

    Fix time readability issue

--- script.js
+++ script.js
@@ -11,19 +11,23 @@ response.then((v) => {
     let start_time = contests[item].start_time
     let end_time = contests[item].end_time
     if (start_time.slice(-3) != 'UTC') {
-      start_time = `${start_time.slice(0, 10)} ${start_time.slice(11, 19)} UTC`
-      end_time = `${end_time.slice(0, 10)} ${end_time.slice(11, 19)} UTC`
+      start_time = `${start_time.slice(0, 10).split("-").reverse().join("-")} at ${start_time.slice(11, 19)} UTC`
+      end_time = `${end_time.slice(0, 10).split("-").reverse().join("-")} at ${end_time.slice(11, 19)} UTC`
+    }
+    else {
+      start_time = `${start_time.slice(0, 10).split("-").reverse().join("-")} at ${start_time.slice(11)}`
+      end_time = `${end_time.slice(0, 10).split("-").reverse().join("-")} at ${end_time.slice(11)}`
     }
     ihtml += `
     <div class="card mx-2 my-2" style="width: 20rem;">
-  <img src="https://source.unsplash.com/random/500x400/?coding,hacking,computer&${i}" class="card-img-top" alt="Coding Event">
-  <div class="card-body">
+    <img src="https://source.unsplash.com/random/500x400/?coding,hacking,computer&${i}" class="card-img-top" alt="Coding Event">
+    <div class="card-body">
     <h5 class="card-title">${contests[item].name}</h5>
     <p class="card-text">Status: ${contests[item].status === 'CODING' ? '<b>RUNNING</b>' : '<b>NOT RUNNING</b>'}</p>
     <p class="card-text">Starts in 24 Hours? ${contests[item].in_24_hours}</p>
     <p class="card-text">Site: ${contests[item].site}</p>
-    <p class="card-text">Starts at: ${start_time}</p>
-    <p class="card-text">Ends at: ${end_time}</p>
+    <p class="card-text">Starts: ${start_time}</p>
+    <p class="card-text">Ends: ${end_time}</p>
     <a href="${contests[item].url}" class="btn btn-primary">Visit Contest</a>
     </div>
   </div>`
